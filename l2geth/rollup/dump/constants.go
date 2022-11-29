5611344a8f7a2f50ca0b957c6e2c90de1862cb6d
corey <94893343+curryxbo@users.noreply.github.com>
2022-11-29 15:05:38 +0800

    mock handle reorg (#361)

--- l2geth/rollup/dump/constants.go
+++ l2geth/rollup/dump/constants.go
@@ -7,3 +7,4 @@ import (
 var BvmEthAddress = common.HexToAddress("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD0000")
 var BvmFeeWallet = common.HexToAddress("0x4200000000000000000000000000000000000011")
 var BvmWhitelistAddress = common.HexToAddress("0x4200000000000000000000000000000000000002")
+var BvmReorgAddress = common.HexToAddress("0xDeadDeAddeAddEAddeadDEaDDEAdDeaDDeAD2222")
